1b710f7fa1ae24b48edc28de314c718f0933da56
Conor <git@dbuidl.com>
2023-02-25 21:24:06 +0000

    Add CSS grid on top of map.

---
+++ frontend/frontend.js
@@ -0,0 +1,10 @@
+let interestPoints = {
+    'port1': [0, 3, "Port 1"],
+    'port2': [5, 8, "Port 2"],
+    'port3': [12, 1, "Port 3"],
+    'oilrig1': [2, 6, "Oil Rig 1"],
+    'oilrig2': [3, 2, "Oil Rig 2"],
+    'oilrig3': [6, 4, "Oil Rig 3"],
+    'oilrig4': [9, 7, "Oil Rig 4"],
+    'oilrig5': [11, 3, "Oil Rig 5"],
+}
